fa81bdb9f92276745751a37122c92a5631e0e87b
Frederic GAURAT <fgaurat@eolem.com>
2020-05-05 16:40:42 +0200

    add reducers

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 
+import { createStore } from 'redux'
+import todos from './reducers/todos.reducer';
+
+
+const store = createStore(todos)
+
 ReactDOM.render(
   <Provider store={store}>
-
     <React.StrictMode>
       <App />
-    </React.StrictMode>  </Provider>,
+    </React.StrictMode>
+  </Provider>,
 
   document.getElementById('root')
 );
